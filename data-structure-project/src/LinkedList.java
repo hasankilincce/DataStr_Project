50c1c30f67b6b888d1503e6d3b302cff6ad74610
Hasan Akgun <akgunhasan8@gmail.com>
2025-04-26 23:23:41 +0300

    search method added for linked list

--- data-structure-project/src/LinkedList.java
+++ data-structure-project/src/LinkedList.java
@@ -14,10 +14,12 @@ public class LinkedList {
 
   //head of the linked list
   private Node head;
-
+  int size = 0;
   //constructor for LinkedList
   public LinkedList() {
     head = null;
+    size = 0;
+
   }
 
   public boolean isEmpty(){
@@ -32,6 +34,7 @@ public class LinkedList {
     Node newNode = new Node(data);
     newNode.next = head;
     head = newNode;
+    size++;
   }
 
   public void addLast(int data){
@@ -44,6 +47,7 @@ public class LinkedList {
     }
 
     current.next=newNode;
+    size++;
   }
 
   public void insertAt(int index, int data){
@@ -55,10 +59,12 @@ public class LinkedList {
     }
     newNode.next = current.next;
     current.next = newNode;
+    size++;
   }
 
   public void removeFirst(){
     head = head.next;
+    size--;
   }
 
   public void removeLast(){
@@ -67,6 +73,7 @@ public class LinkedList {
       current = current.next;
     }
     current.next = null;
+    size--;
   }
 
   public void removeAt(int index){
@@ -75,6 +82,18 @@ public class LinkedList {
       current = current.next;
     }
     current.next = current.next.next;
+    size--;
+  }
+
+  public int search(int data){
+    Node current = head;
+    for(int i=0; i<size; i++){
+      if(current.data == data){
+        return current.data;
+      }
+      current = current.next;
+    }
+    return -1; //not found
   }
 
   public void printElements(){
--- data-structure-project/src/Main.java
+++ data-structure-project/src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
   public static void main(String[] args) {
     LinkedList newList = new LinkedList();
+    LinkedList newList2 = new LinkedList();
     newList.addFirst(0);
     newList.addLast(1);
     newList.addLast(2);
     newList.addLast(3);
     newList.addLast(4);
     newList.addLast(5);
-    newList.insertAt(2,9);
+    newList.insertAt(2,9); 
     newList.printElements();
+
   }
 }
